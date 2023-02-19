57891d9d3dae089dca79f5c6cd65d4712e41ec60
Ishan Sharma <1905614@kiit.ac.in>
2023-02-19 20:40:24 +0530

    UI Changes

--- src/Components/Coin.js
+++ src/Components/Coin.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import 'animate.css'
 
 function Coin({name, icon, price, symbol}) {
   return (
     <div className='coin'>
         <h1>Name : {name}</h1>
+        <div className='animate__animated animate__rotateIn animate__delay-1s'>
         <img src={icon} alt="icon of the coin"/>
+        </div>
         <h3>Price : {price}</h3>
         <h3>Symbol : {symbol}</h3>
     </div>
